ba3ec467296556a8e3b8239f39912865f81738ec
David G <david.giasson@claurendeau.qc.ca>
2025-03-21 11:18:01 -0400

    Remplacement des int par des char dans la classe Vecteur.

--- src/Vecteur.java
+++ src/Vecteur.java
@@ -1,12 +1,12 @@
 public class Vecteur {
-    private int[] tab;
+    private char[] tab;
     private int nbElements;
 
     private static final int RATIO_AGRANDISSEMENT = 2;
     private static final int TAILLE_INITIALE = 2;
 
     public Vecteur() {
-        this.tab = new int[TAILLE_INITIALE];
+        this.tab = new char[TAILLE_INITIALE];
         this.nbElements = 0;
     }
 
